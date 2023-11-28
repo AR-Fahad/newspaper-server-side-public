d32d3d1a05545156752e4c000c55a58f4117becf
AR-Fahad <ashfaqurrahmanfahad@gmail.com>
2023-11-29 02:14:05 +0600

    articles operations by admin added

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,11 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../db/db");
+
+const getArticlesByAdmin = async (req, res) => {
+  const articlesCollection = await getDb().collection("articles");
+  const result = await articlesCollection.find().toArray();
+  res.send(result);
+};
 
 const setArticle = async (req, res) => {
   const article = req.body;
@@ -7,4 +14,45 @@ const setArticle = async (req, res) => {
   res.send(result);
 };
 
-module.exports = { setArticle };
+const updateArticleByAdmin = async (req, res) => {
+  const articleId = req.params.id;
+  const updateArticle = req.body;
+  const articlesCollection = await getDb().collection("articles");
+  const query = { _id: new ObjectId(articleId) };
+  let update = {};
+  let result;
+  if (updateArticle?.status) {
+    update = {
+      $set: {
+        status: updateArticle.status,
+        reasons: updateArticle.reasons,
+        views: 0,
+      },
+    };
+    result = await articlesCollection.updateOne(query, update);
+    return res.send(result);
+  }
+
+  update = {
+    $set: {
+      isPremium: true,
+    },
+  };
+  result = await articlesCollection.updateOne(query, update);
+  return res.send(result);
+};
+
+const deleteArticle = async (req, res) => {
+  const articleId = req.params.id;
+  const query = { _id: new ObjectId(articleId) };
+  const articlesCollection = await getDb().collection("articles");
+  const result = await articlesCollection.deleteOne(query);
+  res.send(result);
+};
+
+module.exports = {
+  setArticle,
+  getArticlesByAdmin,
+  updateArticleByAdmin,
+  deleteArticle,
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const { setUser, getUsers, updateUser } = require("../controllers/users");
-const { setArticle } = require("../controllers/articles");
+const {
+  setArticle,
+  getArticlesByAdmin,
+  updateArticleByAdmin,
+  deleteArticle,
+} = require("../controllers/articles");
 
 const router = express.Router();
 
@@ -10,6 +15,9 @@ router.post("/users", setUser);
 router.patch("/users/:id", updateUser);
 
 // article CRUD
+router.get("/admin/articles", getArticlesByAdmin);
 router.post("/articles", setArticle);
+router.patch("/admin/articles/:id", updateArticleByAdmin);
+router.delete("/articles/:id", deleteArticle);
 
 module.exports = { router };
